b653185bc15376a9519a754ee0becae8f69fff0a
Dimitri Lavrentev <dimitrilavrentev96@gmail.com>
2022-08-03 18:19:01 +0200

    created description css and js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './Components/Header/Header';
 import Carussel from './Components/Body/Carussel/Carussel';
+import Description from './Components/Body/Description/Description';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Carussel />
+      <Description />
     </div>
   );
 }
---
+++ src/Components/Body/Description/Description.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Description.css";
+
+const Description = () => {
+  return (
+    <div className="description-container">
+      <div className="info">
+        <p className="company-p">SNEAKER COMPANY</p>
+        <p className="model-p">Fall Limited Edition Sneakers</p>
+        <p className="text-p">
+          These low-profile sneakers are your perfect casual wear companion.
+          Featuring a durable rubber outer sole, they’ll withstand everything
+          the weather can offer.
+        </p>
+      </div>
+      <div className="price"></div>
+      
+      <div className="add"></div>
+    </div>
+  );
+};
+
+export default Description;
