04aa7bb61735f8ba20ba4e20e3b3c4da7db081cf
Dimitri Lavrentev <dimitrilavrentev96@gmail.com>
2022-08-03 17:23:48 +0200

    created header js and css

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+import Menu from "./../../Images/icon-menu.svg";
+import Logo from "../../Images/logo.svg";
 
 const Header = () => {
   return (
-    <div className='header-container'>
-
+    <div className="header-container">
+      <div className="group-left">
+        <img src={Menu} alt="Menu" />
+        <img src="" alt="Logo" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- style-guide.md
+++ style-guide.md
@@ -17,9 +17,9 @@ The designs were created to the following widths:
 ### Neutral
 
 - Very vark blue: hsl(220, 13%, 13%)
-- Dark grayish blue: hsl(219, 9%, 45%)
-- Grayish blue: hsl(220, 14%, 75%)
-- Light grayish blue: hsl(223, 64%, 98%)
+- Dark grayish blue: hsl(219, 9%, 45%);
+- Grayish blue: hsl(220, 14%, 75%);
+- Light grayish blue: hsl(223, 64%, 98%);
 - White: hsl(0, 0%, 100%)
 - Black (with 75% opacity for lightbox background): hsl(0, 0%, 0%)
 
