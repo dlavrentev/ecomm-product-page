5c3ed2b74132879a758ea27a739e1b1cac2013bc
Dimitri Lavrentev <dimitrilavrentev96@gmail.com>
2022-08-05 21:55:25 +0200

    added slider

--- src/Components/Body/Carussel/Carussel.js
+++ src/Components/Body/Carussel/Carussel.js
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Carussel.css";
-import Image1 from "../../../Images/image-product-1.jpg";
+import Pre from "./../../../Images/icon-previous.svg";
+import Next from "./../../../Images/icon-next.svg";
 
-const Carussel = () => {
-  return <div className="carussel-container">
-    <img className="image-1" src={Image1} alt="Shoes"></img>
-  </div>;
+const Carussel = ({ slides }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const goToPrevious = () => {
+    const isFirstSlide = currentIndex === 0
+    const newIndex = isFirstSlide ? slides.length -1 : currentIndex -1;
+    setCurrentIndex(newIndex);
+  }
+
+  const goToNext = () => {
+    const isLastSlide = currentIndex === slides.length -1;
+    const newIndex = isLastSlide ? 0 : currentIndex +1;
+    setCurrentIndex(newIndex);
+  }
+
+  return (
+    <div className="carussel-container">
+      <div className="previousArrow" onClick={goToPrevious}>
+        <img src={Pre} alt="Previous Button" />
+      </div>
+      <div className="nextArrow" onClick={goToNext}>
+        <img src={Next} alt="Next Button" />
+      </div>
+      <div
+        className="image"
+        style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+      ></div>
+      <div>
+        {slides.map((slide, slideIndex) => (
+          <div key={slideIndex}></div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Carussel;
