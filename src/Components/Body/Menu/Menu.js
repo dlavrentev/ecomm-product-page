15a9e6e49f5c3e57a44090375cac742471e79007
Dimitri Lavrentev <dimitrilavrentev96@gmail.com>
2022-08-07 18:28:12 +0200

    Update Menu.js

--- src/Components/Body/Menu/Menu.js
+++ src/Components/Body/Menu/Menu.js
@@ -4,7 +4,12 @@ import Close from "../../../Images/icon-close.svg"
 const Menu = () => {
   return (
     <div>
-
+      <img src={Close} alt="Close" />
+      <p>Collections</p>
+      <p>Men</p>
+      <p>Woman</p>
+      <p>About</p>
+      <p>Contact</p>
     </div>
   )
 }
