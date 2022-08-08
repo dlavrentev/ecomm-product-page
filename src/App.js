cf579a5ee23a227aef454426789018a236cf5607
Dimitri Lavrentev <dimitrilavrentev96@gmail.com>
2022-08-08 12:59:04 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -2,19 +2,18 @@ import './App.css';
 import Header from './Components/Header/Header';
 import Carussel from './Components/Body/Carussel/Carussel';
 import Description from './Components/Body/Description/Description';
-import Menu from './Components/Body/Menu/Menu';
+
 
 function App() {
   const slides = [
-    {url: 'http://localhost:3001/image-product-1.jpg', title: 'Shoe1'},
-    {url: 'http://localhost:3001/image-product-2.jpg', title: 'Shoe2'},
-    {url: 'http://localhost:3001/image-product-3.jpg', title: 'Shoe3'},
-    {url: 'http://localhost:3001/image-product-4.jpg', title: 'Shoe4'},
+    {url: 'http://localhost:3000/image-product-1.jpg', title: 'Shoe1'},
+    {url: 'http://localhost:3000/image-product-2.jpg', title: 'Shoe2'},
+    {url: 'http://localhost:3000/image-product-3.jpg', title: 'Shoe3'},
+    {url: 'http://localhost:3000/image-product-4.jpg', title: 'Shoe4'},
   ]
   return (
     <div className="App">
       <Header />
-      <Menu />
       <Carussel slides={slides} />
       <Description />
     </div>
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,20 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import Close from "../../Images/icon-close.svg";
 import Burger from "./../../Images/icon-menu.svg";
 import Logo from "../../Images/logo.svg";
 import Cart from "../../Images/icon-cart.svg";
 import Profile from "../../Images/image-avatar.png";
-import './Header.css'
+import "./Header.css";
 
 const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const closeBtn = () => {
+    setShowMenu(false);
+  };
+
+  const openBtn = () => {
+    setShowMenu(true);
+  };
+
   return (
-    <div className="header-container">
-      <div className="group-left">
-        <img className="menu" src={Burger} alt="Menu" />
-        <img className="logo" src={Logo} alt="Logo" />
-      </div>
-      <div className="group-right">
-      <img className="cart" src={Cart} alt="Cart" />
-        <img className="profile" src={Profile} alt="Profile" height={24}/>
+    <div className="Header">
+      {showMenu ? (
+        <div className="menu-overlay">
+        <div className="menu-container">
+          <img src={Close} alt="Close" onClick={closeBtn} />
+          <div>
+            <p>Collections</p>
+            <p>Men</p>
+            <p>Woman</p>
+            <p>About</p>
+            <p>Contact</p>
+          </div>
+        </div>
+        <div className="opacity"></div>
+        </div>
+        
+      ) : null}
+
+      <div className="header-container">
+        <div className="group-left">
+          <img className="menu" src={Burger} onClick={openBtn} alt="Menu" />
+          <img className="logo" src={Logo} alt="Logo" />
+        </div>
+        <div className="group-right">
+          <img className="cart" src={Cart} alt="Cart" />
+          <img className="profile" src={Profile} alt="Profile" height={24} />
+        </div>
       </div>
     </div>
   );
