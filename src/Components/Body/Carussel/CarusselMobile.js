0f9f77e3fd0a168474e1d9200e6e8bc9f7c46e35
Dimitri Lavrentev <dimitrilavrentev96@gmail.com>
2022-08-15 15:17:25 +0200

    created carusselmobile

---
+++ src/Components/Body/Carussel/CarusselMobile.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const CarusselMobile = () => {
+  return (
+    <div>
+        <img src="" alt="" />
+        <div>
+            <img src="" alt="" />
+            <img src="" alt="" />
+            <img src="" alt="" />
+            <img src="" alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default CarusselMobile
